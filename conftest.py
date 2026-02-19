ef545041a39cd450a68ba9af6777934f6bfda995
Gamzat <gamzat27@icloud.com>
2026-02-19 03:28:26 +0300

    added CustomRequester

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 # Фикстуры для Pytest
 
 import requests
-from constants import BASE_URL_AUTH, HEADERS, REGISTER_ENDPOINT, LOGIN_ENDPOINT
 import pytest
+from faker import Faker
+from constants import BASE_URL_AUTH, REGISTER_ENDPOINT
 from utils.data_generator import DataGenerator
+from custom_requester.custom_requester import CustomRequester
 
+faker = Faker("ru_RU")
 
 @pytest.fixture(scope="session")
 def my_test_user():
@@ -25,26 +28,28 @@ def my_test_user():
 
 
 @pytest.fixture(scope="session")
-def auth_session(my_test_user):
-    # Регистрируем нового пользователя
-    register_url = f"{BASE_URL_AUTH}{REGISTER_ENDPOINT}"
-    response = requests.post(register_url, json=my_test_user, headers=HEADERS)
-    assert response.status_code == 201, "Ошибка регистрации пользователя"
+def registered_user(requester, my_test_user):
+    """
+    Фикстура для регистрации и получения данных зарегистрированного пользователя.
+    """
+    response = requester.send_request(
+        method="POST",
+        endpoint=REGISTER_ENDPOINT,
+        data=my_test_user,
+        expected_status=201
+    )
+    response_data = response.json()
+    registered_user = my_test_user.copy()
+    registered_user["id"] = response_data["id"]
+    return registered_user
 
-    # Логинимся для получения токена
-    login_url = f"{BASE_URL_AUTH}{LOGIN_ENDPOINT}"
-    login_data = {
-        "email": my_test_user["email"],
-        "password": my_test_user["password"]
-    }
-    response = requests.post(login_url, json=login_data, headers=HEADERS)
-    assert response.status_code == 200, "Ошибка авторизации"
 
-    # Получаем токен и создаём сессию
-    token = response.json().get("accessToken")
-    assert token is not None, "Токен доступа отсутствует в ответе"
-
+@pytest.fixture(scope="session")
+def requester():
+    """
+    Фикстура для создания экземпляра CustomRequester.
+    """
     session = requests.Session()
-    session.headers.update(HEADERS)
-    session.headers.update({"Authorization": f"Bearer {token}"})
-    return session
+    return CustomRequester(session=session, base_url=BASE_URL_AUTH)
+
+
